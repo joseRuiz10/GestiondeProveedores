65b5ce342a96eb322ee7b013ea6298b53a074d0b
Jose Ruiz <joseemanuelruiz010@gmail.com>
2020-11-02 14:08:08 -0300

    Primer commit

---
+++ TFIGestionProveedores03/Controllers/EquipoesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using TFIGestionProveedores03;
+
+namespace TFIGestionProveedores03.Controllers
+{
+    public class EquipoesController : Controller
+    {
+        private dbGestionProveedores02Entities db = new dbGestionProveedores02Entities();
+
+        // GET: Equipoes
+        public ActionResult Index(string descripcion)
+        {
+            var equipos = db.Equipo.Where<Equipo>(e => e.descripcion.Contains(descripcion)).Include(p => p.Proveedor);
+            if (descripcion.IsEmpty())
+            {
+                equipos = db.Equipo.Include(e => e.Proveedor);
+            }
+            return View(equipos.ToList());
+        }
+
+        
+
+        // GET: Equipoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipo);
+        }
+
+        // GET: Equipoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.idProveedor = new SelectList(db.Proveedor, "idProveedor", "RazonSocial");
+            return View();
+        }
+
+        // POST: Equipoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idEquipo,descripcion,marca,modelo,color,Fecha_Adqu,Fecha_FinGarantia,idProveedor")] Equipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Equipo.Add(equipo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idProveedor = new SelectList(db.Proveedor, "idProveedor", "RazonSocial", equipo.idProveedor);
+            return View(equipo);
+        }
+
+        // GET: Equipoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idProveedor = new SelectList(db.Proveedor, "idProveedor", "RazonSocial", equipo.idProveedor);
+            return View(equipo);
+        }
+
+        // POST: Equipoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idEquipo,descripcion,marca,modelo,color,Fecha_Adqu,Fecha_FinGarantia,idProveedor")] Equipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(equipo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idProveedor = new SelectList(db.Proveedor, "idProveedor", "RazonSocial", equipo.idProveedor);
+            return View(equipo);
+        }
+
+        // GET: Equipoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipo);
+        }
+
+        // POST: Equipoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Equipo equipo = db.Equipo.Find(id);
+            db.Equipo.Remove(equipo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TFIGestionProveedores03/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TFIGestionProveedores03.MvcApplication" Language="C#" %>
